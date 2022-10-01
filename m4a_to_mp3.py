ced8ae6d6920c82d22882daa516ac23f38a6b994
片岡勇輝 <kayuyuuki0825@icloud.com>
2022-10-02 02:18:23 +0900

    :fire: 不要なファイルを削除

--- m4a_to_mp3.py
+++ m4a_to_mp3.py
@@ -2,7 +2,7 @@ import os
 import ffmpeg
 from tkinter import filedialog, messagebox
 
-messagebox.showinfo("message", "mp3形式に変換するm4aファイルを選択してください。")
+messagebox.showinfo("message", "Choose the .m4a file you want to convert to .mp3 file.")
 input_file_path = filedialog.askopenfilename()
 input_file_name, input_file_ext = os.path.splitext(os.path.basename(input_file_path))
 
